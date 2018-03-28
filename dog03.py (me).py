b3ef319b80445a0902e7cf65295c5530b86e543e
Tracer64 <36674282+Tracer64@users.noreply.github.com>
2018-03-28 17:04:32 -0400

    Update dog03.py (me).py

--- dog03.py (me).py
+++ dog03.py (me).py
@@ -28,5 +28,7 @@ print(x.weight)
 d.add_weight(9)
 #d.add_weight(9) will add 9 to the original function for weight set at 40 to set the current weight to 49
 
-x.add_weight(9)
+x.add_weight(1)
 #x.add_weight(1) will add 1 to the original function for weight set at 101 to set the current weight to 110
+
+#Don't forget to add the weight "CHANGES" remember that the changes didn't take effect when you last ran the program.
